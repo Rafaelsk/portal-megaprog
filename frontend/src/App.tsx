1e7a8906f7f09db363d880a048688d61a961a671
Rafael Robles <megaprog@gmail.com>
2023-08-18 19:51:14 +0200

    React practice

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  let heading = 'German Cities';
+  let items = [
+    'Berlin',
+    'Hamburg',
+    'München',
+    'Bremen',
+    'Frankfurt',
+  ];
+  const eventHandler = (item: string) => {
+    console.log(item);
+  };
+
+  // return <div><ListGroup items={items} heading={heading} onSelectItem={eventHandler}/></div>;
+  return <div><Alert><span>This is my cool message</span></Alert></div>;
 }
 
 export default App;
